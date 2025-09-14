5ba6679bb2193371b49c95e5891f40490e149978
Ztaclie <melmeh42@gmail.com>
2025-09-14 11:11:12 +0300

    basic vite&tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-5xl font-bold underline text-green-500">
+          Hello World
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
