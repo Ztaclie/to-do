a07e0678fc58dc5ef0e51ee76e88533162c8c0ad
Ztaclie <melmeh42@gmail.com>
2025-09-14 11:15:47 +0300

    basic daisyui setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 const App = () => {
   return (
     <>
-      <div className="App">
+      <div className="App flex flex-col items-center justify-center gap-7 h-screen bg-gray-100">
         <h1 className="text-5xl font-bold underline text-green-500">
           Hello World
         </h1>
+        <div
+          className="radial-progress text-green-500"
+          style={{ "--value": 70 }}
+          aria-valuenow={70}
+          role="progressbar"
+        >
+          70%
+        </div>
       </div>
     </>
   );
