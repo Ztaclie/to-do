5b68a33c3eeb9f82d726146bcca2c5625a92aaa4
Ztaclie <melmeh42@gmail.com>
2025-09-14 11:20:38 +0300

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <>
+      <Navbar />
       <div className="App flex flex-col items-center justify-center gap-7 h-screen bg-gray-100">
         <h1 className="text-5xl font-bold underline text-green-500">
           Hello World
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">To Do</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
